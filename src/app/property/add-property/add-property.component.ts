4595e9ba0ba7d91c2d573c8bff894b882ac0465b
rbrtj <robertjaszczurek@icloud.com>
2023-06-05 21:49:15 +0200

    Property form UI

--- src/app/property/add-property/add-property.component.ts
+++ src/app/property/add-property/add-property.component.ts
@@ -1,17 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { Router } from '@angular/router';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-add-property',
   templateUrl: './add-property.component.html',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    CommonModule,
+  ],
 })
 export class AddPropertyComponent {
+  @ViewChild('form') addPropertyForm!: NgForm;
   constructor(private router: Router) {}
 
   onBack(): void {
     this.router.navigate(['/']);
+  }
+
+  onSubmit() {
+    console.log(this.addPropertyForm);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { importProvidersFrom } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { APP_ROUTE } from './app/app.route';
-
+import { provideAnimations } from '@angular/platform-browser/animations';
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(HttpClientModule), provideRouter(APP_ROUTE)],
+  providers: [
+    importProvidersFrom(HttpClientModule),
+    provideRouter(APP_ROUTE),
+    provideAnimations(),
+  ],
 }).catch((err) => console.error(err));
